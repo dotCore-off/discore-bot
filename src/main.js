a3c58d389df36e1c04164c958182e09afbca9032
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2021-02-04 21:58:06 +0100

    Updated some notes

--- src/main.js
+++ src/main.js
@@ -1,58 +1,66 @@
-// Built-in libraries from Node.JS
+// Import libraries
 const path = require('path')
 const fs = require('fs')
-// Only import the Client class from Discord.js
 const { Client, Intents } = require('discord.js')
 
+// Let's require a random file
 require('dotenv').config()
 
-// Super fancy config loader/validator
+// Loading config
 const config = (() => {
+    // Bot token
     const token = process.env.BOT_TOKEN
 
-    // If there isn't a token, the bot won't start, but if there is then
-    // we want to make sure it's a valid bot token
+    // In case there's no token
     if (!token) {
         console.error('Missing BOT_TOKEN environment variable')
         process.exit(1)
     }
 
+    // In case token is invalid
     if (!/^[a-zA-Z0-9_.-]{59}$/.test(token)) {
         console.error('Invalid bot token!')
         process.exit(1)
     }
 
+    // Bot prefix
     const prefix = process.env.BOT_PREFIX
 
+    // In case there's no prefix
     if (!prefix) {
         console.error('Missing BOT_PREFIX environment variable')
         process.exit(1)
     }
 
+    // We return both token and prefix
     return { token, prefix }
 })()
 
-// Store the commands in a Map (slightly better than a raw object)
+// Storing commands
 const commands = new Map()
-// Define gateway intents
+
+// Define intents
 const intents = new Intents([
-    Intents.NON_PRIVILEGED, // include all non-privileged intents, would be better to specify which ones you actually need
-    "GUILD_MEMBERS", // lets you request guild members
+    Intents.NON_PRIVILEGED,
+    "GUILD_MEMBERS",
 ]);
+
 // Create the client
 const bot = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'], ws: { intents }, fetchAllMembers: true, disableEveryone: true })
 
-// Store the config and commands on the bot variable so as to make them
-// easily accessible in commands and other files
+// Store the config and commands on the bot variable
 bot.config = config
 bot.commands = commands
 
-// Read every file in ./commands and filter out the non-JS files
+// Read directories function
 fs.readdirSync(path.resolve(__dirname, 'commands'))
+    // We filter file's types
     .filter(f => f.endsWith('.js'))
+
+    // Loading loop
     .forEach(f => {
         // Attempt to load the file
-        console.log(`Loading command ${f}`)
+        console.log(`Discore - Loading command ${f}`)
         try {
             // Require the raw file
             let command = require(`./commands/${f}`)
@@ -70,63 +78,45 @@ fs.readdirSync(path.resolve(__dirname, 'commands'))
         }
     })
 
+// Ready event
 bot.on('ready', () => {
+    // Cool log
     console.log(`Logged in as ${bot.user.tag} (ID: ${bot.user.id})`)
+
+    // Bot's permissions
     bot.generateInvite({ permissions: [
-        'SEND_MESSAGES',
-        'MANAGE_MESSAGES',
-        // Here are some other common permissions you might want to include:
-        // (Complete list can be found at https://discord.js.org/#/docs/main/stable/class/Permissions?scrollTo=s-FLAGS)
-        //
-        // *** General moderation permissions:
-        // 'KICK_MEMBERS',
-        // 'BAN_MEMBERS',
-        // *** Guild settings permissions:
-        // 'MANAGE_CHANNELS',
-        // 'MANAGE_GUILD',
-        // 'MANAGE_NICKNAMES',
-        // 'MANAGE_ROLES',
-        // *** Voice permissions:
-        // 'CONNECT',
-        // 'SPEAK',
-        // *** Voice moderation permissions:
-        // 'MOVE_MEMBERS',
-        // 'MUTE_MEMBERS',
-        // 'DEAFEN_MEMBERS',
+        'ADMINISTRATOR'
     ]}).then(invite => {
-        // After generating the invite, log it to the console
+        // Logging invitation link
         console.log(`Click here to invite the bot to your guild:\n${invite}`)
     })
 })
 
+// Message event
 bot.on('message', message => {
-    // Ignore messages from bots and from DMs (non-guild channels)
+    // Ignore messages from bots and from DMs
     if (message.author.bot || !message.guild) {
         return
     }
 
-    // Just a shorthand variable
+    // Noob var
     let { content } = message
-    // Ignore any messages that don't start with the configurable prefix
+
+    // Ignore if prefix isn't used
     if (!content.startsWith(config.prefix)) {
         return
     }
 
-    // Take all the text after the prefix and split it into an array,
-    // splitting at every space (so 'hello world' becomes ['hello', 'world'])
+    // Noob vars
     let split = content.substr(config.prefix.length).split(' ')
-    // Get the command label (which is the first word after the prefix)
     let label = split[0]
-    // Get the rest of the words after the prefix
     let args = split.slice(1)
 
-    // If there's a command with that given label...
+    // In case there's a label
     if (commands.get(label)) {
-        // ... get the command with that label and run it with the bot, the
-        // message variable, and the args as parameters
         commands.get(label).run(bot, message, args)
     }
 })
 
-// Only run the bot if the token was provided
+// Launch bot if there's a valid token
 config.token && bot.login(config.token)
