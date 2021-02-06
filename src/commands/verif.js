ddb80f85d16374c0353f84236d1aa83be2347bc7
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2021-02-06 20:35:15 +0100

    Update verif.js

--- src/commands/verif.js
+++ src/commands/verif.js
@@ -55,14 +55,14 @@ exports.run = (bot, msg, args) => {
 
         // If it's not the right person
         if (user.id !== m.author.id) {
-            message.delete({ timeout : 10 }).catch(console.error);
+          m.delete({ timeout : 10 }).catch(console.error);
           return;
         }
  
         // If the entered code is wrong
-        if (!message.content.startsWith(picked_code) && user.id === message.author.id || !message.content && has_exp !== 0 && attempts !== 1 && user.id === message.author.id) {
+        if (!m.content.startsWith(picked_code) && user.id === m.author.id || !m.content && has_exp !== 0 && attempts !== 1 && user.id === m.author.id) {
           // Delete user attempt
-          message.delete({ timeout : 10 }).catch(console.error);
+          m.delete({ timeout : 10 }).catch(console.error);
           
           // In case there's remaining attempts > Continue + Delete one attempt
           if (attempts !== 0) {
@@ -81,12 +81,12 @@ exports.run = (bot, msg, args) => {
         }
 
         // If everything is right - Proceed
-        if (message.content.startsWith(picked_code) && has_exp === 0 && attempts !== 0 && user.id === message.author.id) {
+        if (m.content.startsWith(picked_code) && has_exp === 0 && attempts !== 0 && user.id === m.author.id) {
           // Delete user attempt
-          message.delete({ timeout : 10 }).catch(console.error);
+          m.delete({ timeout : 10 }).catch(console.error);
           // Edit code embed + Delete with a timeout
-          message.edit(code_message.setAuthor("Captcha code ❱ ✔️", `${message.author.displayAvatarURL(format = 'png', dynamic = true)}`).setDescription("``✔️ Code validé / Good code``").setColor(0x33FF00).setFooter("Bienvenue sur Waurum / Welcome on Waurum")).catch(console.error);
-          message.delete({ timeout : 10000 }).catch(console.error);
+          m.edit(code_message.setAuthor("Captcha code ❱ ✔️", `${m.author.displayAvatarURL(format = 'png', dynamic = true)}`).setDescription("``✔️ Code validé / Good code``").setColor(0x33FF00).setFooter("Bienvenue sur Waurum / Welcome on Waurum")).catch(console.error);
+          m.delete({ timeout : 10000 }).catch(console.error);
           // Should add the role addition here
           active_captcha = 0;
         }   
