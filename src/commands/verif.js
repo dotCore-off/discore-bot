105e56347ef81205de049d437aa699d0e4b6b026
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2021-02-07 16:38:05 +0100

    Small fix with configuration file

--- src/commands/verif.js
+++ src/commands/verif.js
@@ -8,7 +8,7 @@ module.exports.run = (bot, msg, args) => {
   // Checking if there's no other verif
   if (active_captcha === 0) {
     // Noob var
-    const good_c = msg.guild.channels.cache.find(ch => ch.name === cfg.captchan);
+    const good_c = msg.guild.channels.cache.find(ch => ch.name === config.captchan);
 
     // In case it's not in the good channel
     if (msg.channel.id !== good_c.id) {
