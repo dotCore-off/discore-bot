e6f8c8cb8f91b270e13ee393b58ae82472eec65d
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2021-02-11 19:05:26 +0100

    Updated a broken var that caused footer to not appear

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -2,23 +2,23 @@
 const Discord = require('discord.js');
 const config = require('../../config.json');
 
-// avatar function
+// Avatar function
 exports.run = (bot, msg, args) => {
-var avatar = msg.author;
+  var avatar = msg.author;
 
-    // Delete command
-    msg.delete({ timeout : 10 }).catch(console.error);
+  // Delete command
+  msg.delete({ timeout : 10 }).catch(console.error);
 
-    // Create Embed
-    const embed = new Discord.MessageEmbed()
-      .setTitle(":kissing_heart: ❱ Avatar system.")
-      .setColor(0x3898FF)
-      .setDescription(`${avatar} ❱ Voici votre pp`)
-      .setImage(`${avatar.displayAvatarURL(format = 'gif, png', size = 1024)}`)
-    msg.channel.send(embed).catch(console.error).then(m => { m.delete({ timeout : 10000 }) }); 
-    
+  // Create Embed
+  const embed = new Discord.MessageEmbed()
+    .setTitle(":kissing_heart: ❱ Avatar system.")
+    .setColor(0x3898FF)
+    .setDescription(`${avatar} ❱ Voici votre pp`)
+    .setImage(`${avatar.displayAvatarURL(format = 'gif, png', size = 1024)}`)
+  msg.channel.send(embed).catch(console.error).then(m => { m.delete({ timeout : 10000 }) });   
 }
-// Help help
+
+// Avatar help
 exports.help = {
     name: 'avatar',
     usage: 'avatar',
