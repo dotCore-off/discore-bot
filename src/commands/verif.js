aac2566c99e42d2d36103370dc3bee404bf250f4
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2021-03-13 17:02:12 +0100

    Update verif.js

--- src/commands/verif.js
+++ src/commands/verif.js
@@ -90,10 +90,10 @@ module.exports.run = (bot, msg, args) => {
         const logs_verif = new Discord.MessageEmbed()
           .setAuthor(`${author} ❱ Verify Logs`,`${msg.author.displayAvatarURL(format = 'png', dynamic = true)}`)
           .setColor(0x3898FF)
-          .addField("✅ ❱ Verified member :", kickMember.user)
+          .addField("✅ ❱ Verified member :", author)
           .addField("📆 ❱ Date :", message.createdAt.toLocaleString())
           .setFooter(config.trademark, config.author_icon)
-          
+
         const channel = message.guild.channels.cache.find(ch => ch.name === '📟┃logs-waurum');
         channel.send(logs_verif);
         return;
